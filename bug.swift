cae69e5780d010f9a1c3d1e789164baa40d476b5
BugHunter-X <askzenai@gmail.com>
2025-01-15 00:12:06 +0100

    Add bug.swift file

---
+++ bug.swift
@@ -0,0 +1,12 @@
+func fetchData(completion: @escaping (Result<[String], Error>) -> Void) {
+    // Simulate network request
+    DispatchQueue.global().asyncAfter(deadline: .now() + 2) { 
+        let data = ["item1", "item2", "item3"]
+        // Simulate a network error
+        if arc4random_uniform(2) == 0 {  // 50% chance of error
+            completion(.failure(NSError(domain: "NetworkError", code: 1, userInfo: nil)))
+        } else {
+            completion(.success(data))
+        }
+    }
+}
